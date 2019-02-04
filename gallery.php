2e1f1cc85e0565ea8e6941d842a4f3b4db5c36ad
Ian CALVERT <icalvert@c2r2s3.wethinkcode.co.za>
2019-02-04 14:31:13 +0200

    working on deleting images

--- gallery.php
+++ gallery.php
@@ -11,13 +11,32 @@ $sql = "SELECT * FROM images WHERE imagename='cheesey'";
 $okay = $connection->prepare($sql);
 $okay->execute();
 $result = $okay->fetchAll();
-
+$likecount = "0";
+$_SESSION['uid'] = $uid;
 
 foreach ($result as $fat=>$value)  {
+
     echo '<img src="'. $value['image'] .'"/>';
+    echo "\n";
+    echo "<form action='gallery.php' method='POST'>
+            <button name='like'>Like($likecount)</button>
+            <button name='delete'>Delete</button>
+            <input type='hidden' name=$uid>
+            <textarea name='comment'></textarea>
+            <button type='submit' name='submitcom'>Comment</button>
+            </form>";
 }
-echo "<img src='getimage.php'>";
 
+if (isset($_POST['like'])) {
+    $likecount++;
+}
 
+if (isset($_POST['delete'])) {
+    $query = "SELECT * FROM images WHERE imagename='cheesey'";
+    $okay = $connection->prepare($query);
+    $okay->execute();
+    $result = $okay->fetch(PDO::FETCH_ASSOC);
+    $image = $result['image'];
+}
 
 ?>
