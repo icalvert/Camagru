ea6149a77ad9ddd6321855290045730bf375742d
Ian CALVERT <icalvert@c2r2s2.wethinkcode.co.za>
2019-02-07 10:21:21 +0200

    likes working

--- gallery.php
+++ gallery.php
@@ -12,34 +12,46 @@ $okay = $connection->prepare($sql);
 $okay->execute();
 $result = $okay->fetchAll();
 //$likes = $_POST['likes'];
-$likes = $result['likes'];
+// print_r($result);
+
+// echo $likes;
 $_SESSION['uid'] = $uid;
+$comment = $_POST['comment'];
+
 
 foreach ($result as $fat=>$value)  {
-
+    $likes = $value['likes'];
+    $id = $value['id'];
+    echo $id;
     echo '<img src="'. $value['image'] .'"/>';
     echo "\n";
     echo "<form action='gallery.php' method='POST'>
-            <button name='like'>Like($likes)</button>
+            <button name='like' value=$id>Like($likes)</button>
             <button name='delete'>Delete</button>
             <input type='hidden' name=$uid>
-            <textarea name='comment'></textarea>
+            <textarea name='comment' value=$id></textarea>
             <button type='submit' name='submitcom'>Comment</button>
             </form>";
+   echo $comment;
 }
 
 if (isset($_POST['like'])) {
-    $query = "SELECT * FROM images WHERE imagename='cheesey'";
+    // print_r($_POST);
+    $id = $_POST['like'];
+    $query = "SELECT * FROM images WHERE id=$id";
     $okay = $connection->prepare($query);
     $okay->execute();
     $result = $okay->fetch(PDO::FETCH_ASSOC);
+    // print_r($result);
     $id = $result['id'];
-    $sqlquery = "UPDATE `images` SET likes='$likes+1' WHERE id='$id'";
+    $image = $result['image'];
+    $likes = $result['likes'];
+    $sqlquery = "UPDATE `images` SET likes=likes+1 WHERE id='$id' OR image='$image'";
     //print_r($result['likes']);
-
+    // echo $sqlquery;
     $yes = $connection->prepare($sqlquery);
     $yes->execute();
-    header("Refresh:10");
+    header("Location: gallery.php");
 }
 
 if (isset($_POST['delete'])) {
@@ -55,13 +67,14 @@ if (isset($_POST['delete'])) {
 }
 
 if (isset($_POST['submitcom'])) {
-    $query = "SELECT * FROM images WHERE imagename='cheesey'";
+    $query = "SELECT * FROM images WHERE id=$id";
     $yes = $connection->prepare($query);
     $yes->execute();
     $result = $yes->fetch(PDO::FETCH_ASSOC);
     $id = $result['id'];
+    $image = $result['image'];
     $comment = $_POST['comment'];
-    $yeah = "UPDATE `images` SET comments='$comment' WHERE id='$id'";
+    $yeah = "UPDATE `images` SET comments='$comment' WHERE image='$image'";
     $hello = $connection->prepare($yeah);
     $hello->execute();
     print_r($comment);
